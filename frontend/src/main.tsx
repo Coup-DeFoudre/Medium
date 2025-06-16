b4be479ba3bb8e87f2e19f142d1597b4b622a64f
Rishabh Pathak <iamrishabhpathak@gmail.com>
2025-06-16 14:24:42 +0530

    Version 2.0.2 Analytics added

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,12 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { UserProvider } from './context/UserContext.tsx'
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <UserProvider>
     <App />
+    <Analytics/>
+     <SpeedInsights />
     </UserProvider>
   </StrictMode>,
 )
