c540ad25cdb871f89d4985bd53eff0c6b3c140ae
Rishabh Pathak <iamrishabhpathak@gmail.com>
2025-06-16 14:27:21 +0530

    Version 2.0

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,14 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { UserProvider } from './context/UserContext.tsx'
-import { Analytics } from "@vercel/analytics/next"
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <UserProvider>
     <App />
-    <Analytics/>
    
     </UserProvider>
   </StrictMode>,
